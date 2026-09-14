c521858023400162efb9ffd9ccff061f285e7cb9
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2026-09-14 15:56:11 -0600

    Fixes #257
    Add first-brush splash screen.

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -3,6 +3,7 @@
 #include <QApplication>
 #include <QFile>
 #include <QFontDatabase>
+#include <QPointer>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickWindow>
@@ -31,6 +32,28 @@ int main(int argc, char* argv[]) {
 
     SolTrace::GUI::App::initialize_logging_handler();
 
+    // Start up splash screen
+
+    QPointer<QQmlApplicationEngine> splash_engine = new QQmlApplicationEngine();
+
+    // If the splash fails, we exit.
+    QObject::connect(
+        splash_engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+
+    // Load splash content
+    splash_engine->load("qrc:/qt/qml/SolTrace/ui/app/Splash.qml");
+
+    // A few spins to load up content
+    app.processEvents();
+    app.processEvents();
+
+
+    // Initialize fonts, etc.
+
     /*
     #ifdef QT_QML_DEBUG
         QQmlDebuggingEnabler::startTcpDebugServer(
@@ -47,6 +70,7 @@ int main(int argc, char* argv[]) {
         if (result < 0) { qWarning() << "Unable to load font:" << font; }
     }
 
+    // Main engine
     QQmlApplicationEngine engine;
 
     QObject::connect(
@@ -55,6 +79,14 @@ int main(int argc, char* argv[]) {
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+
+    QObject::connect(&engine,
+                     &QQmlApplicationEngine::objectCreated,
+                     splash_engine,
+                     &QObject::deleteLater,
+                     Qt::QueuedConnection);
+
+    // engine.
 
     engine.loadFromModule("SolTrace", "Main");
 
